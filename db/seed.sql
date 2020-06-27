a6ffb3aa6a4d205f9668593b8f1f4c0a3c58f734
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-06-27 13:51:24 -0700

    created SQL db files

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Whopper");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("Double-Double");
+INSERT INTO burgers (burger_name) VALUES ("Western Bacon Cheese");
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require("express");
+// Require express dependency
+let express = require("express");
 
-var PORT = process.env.PORT || 8080;
+// Set port
+let PORT = process.env.PORT || 8080;
 
-var app = express();
+// Create variable to use express
+let app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,14 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
+let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
-
+let routes = require("./controllers/burgerController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
